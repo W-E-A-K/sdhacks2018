9b2b57614fe250ebbbb6bf71d93fd51b41b2a874
kchang264 <kellyckc2017@gmail.com>
2018-10-13 15:34:30 -0700

    Draws a rectangle on the canvas

---
+++ GUI/src/CirclePanel.java
@@ -0,0 +1,47 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+public class CirclePanel extends JPanel implements KeyListener {
+
+	public CirclePanel() { 
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		//set size of panel 
+		this.setPreferredSize(new Dimension(2500, 1000));		
+		addKeyListener(this);
+	}
+	
+	public void paintComponent(Graphics g) {
+        super.paintComponent(g);       
+
+        // Draw Text
+        g.drawString("This is my custom Panel!",10,20);
+    }  
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	} 
+	
+}
---
+++ GUI/src/Circling.java
@@ -0,0 +1,25 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.DebugGraphics;
+
+@SuppressWarnings("serial")
+public class Circling extends Canvas { 
+//	DebugGraphics dg; 
+	
+	public Circling() { 
+		//dg = new DebugGraphics(); 
+		setSize(3000,1500); 
+		setBackground( Color.PINK );
+	}
+	
+	@Override 
+	public void paint(Graphics g) { 
+		Graphics2D g2; 
+		g2 =  (Graphics2D) g; 
+		g.drawRect(100, 100, 100, 150);
+	}
+	
+}
