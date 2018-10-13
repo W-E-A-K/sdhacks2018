ce4c735f2392faa43f9aea3bdfe0e759f4023c79
Anna Qiu <annaq1@uci.edu>
2018-10-13 02:04:07 -0700

    initial clock file

---
+++ Microduino/clock/clock.ino
@@ -0,0 +1,23 @@
+#include <Microduino_RTC.h>
+
+
+RTC rtc;
+
+/* DateTime 
+ * uint16_t year
+ * uint8_t month, weekday, day, hour, minute, second
+ * Hardcode DateTime for now, but if possible try to get updated date from some source
+ */
+DateTime dateTime = {2018, 10, 6, 13, 12, 0, 0};
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
