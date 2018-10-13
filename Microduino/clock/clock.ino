88d1c05fde9227ba6cffe2ff7e01c9b2b644e42c
Anna Qiu <annaq1@uci.edu>
2018-10-13 02:09:44 -0700

    testing time

--- Microduino/clock/clock.ino
+++ Microduino/clock/clock.ino
@@ -14,10 +14,27 @@ DateTime dateTime = {2018, 10, 6, 13, 12, 0, 0};
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  // Clear all registers
+  rtc.begin();
+  rtc.clearAll();
+  // Set startup time
+  rtc.setDateTime(dateTime);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  rtc.getDateTime(&dateTime);
+  Serial.print(dateTime.year);
+  Serial.print("/");
+  Serial.print(dateTime.month);
+  Serial.print("/");
+  Serial.print(dateTime.day);
+  Serial.print("     ");
+  Serial.print(dateTime.hour);
+  Serial.print(":");
+  Serial.print(dateTime.minute);
+  Serial.print(":");
+  Serial.print(dateTime.second);
+  Serial.print("\r\n");
 }
