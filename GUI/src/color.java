d68bec9ee850724439088d0c12baadad37f58efb
Anna Qiu <annaq1@uci.edu>
2018-10-13 13:37:16 -0700

    java swing

---
+++ GUI/src/color.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class color {
+	public static void main(String[] args)
+	{
+		JFrame f = new JFrame();
+		f.setSize(1000,1000);//400 width and 500 height  
+		f.setLayout(null);//using no layout managers  
+		f.setVisible(true);//making the frame visible  
+	}
+}
